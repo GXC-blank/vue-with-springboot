be64c9b4d7d2abb8f970560a021852463a9e3eec
Blank <54304937+GXC-blank@users.noreply.github.com>
2020-08-31 11:04:09 +0800

    Add files via upload

---
+++ vue-src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+import axios from 'axios'
+import './axios.js' // 请求拦截
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import 'github-markdown-css/github-markdown.css'
+import './permission.js' // 路由拦截
+// use
+Vue.use(mavonEditor)
+Vue.prototype.$axios = axios //
+Vue.use(Element)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ vue-src/views/Login.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>
+        <router-link to="/blogs">
+        <img src="https://www.markerhub.com/dist/images/logo/markerhub-logo.png" style="height: 60%; margin-top: 10px;">
+        </router-link>
+      </el-header>
+      <el-main>
+        <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="用户名" prop="username">
+            <el-input type="text" maxlength="12" v-model="ruleForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        ruleForm: {
+          password: '111111',
+          username: 'markerhub'
+        },
+        rules: {
+          password: [
+            {validator: validatePass, trigger: 'blur'}
+          ],
+          username: [
+            {required: true, message: '请输入用户名', trigger: 'blur'},
+            {min: 3, max: 12, message: '长度在 3 到 12 个字符', trigger: 'blur'}
+          ]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        const _this = this
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            // 提交逻辑
+            this.$axios.post('http://localhost:8081/login', this.ruleForm).then((res)=>{
+              const token = res.headers['authorization']
+              _this.$store.commit('SET_TOKEN', token)
+              _this.$store.commit('SET_USERINFO', res.data.data)
+              _this.$router.push("/blogs")
+            })
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    },
+    mounted() {
+      this.$notify({
+        title: '看这里：',
+        message: '12',
+        duration: 1500
+      });
+    }
+  }
+</script>
